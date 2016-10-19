5a96d4d84cedcf2a743966ab49ef9e60195e1a9f
casper oakley <casper_oakley@hotmail.com>
2016-10-19 15:41:15 +0100

    Setup basic portaudio hello world

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <stdio.h>
+#include "libs/portaudio.h"
+
+
+void error(PaError err) {
+    Pa_Terminate();
+    fprintf( stderr, "Error number: %d\n", err);
+    fprintf( stderr, "Error message: %s\n", Pa_GetErrorText(err));
+}
+
 
 int main(int argc, char* argv[]) {
+    printf("Hello, World!");
+    PaError err;
+    
+    err = Pa_Initialize();
+
+    if( err != paNoError) {
+        error(err);
+        return 1;
+    }
+
+    printf( "PortAudio version: 0x%08X\n", Pa_GetVersion());
     return 0;
 }
